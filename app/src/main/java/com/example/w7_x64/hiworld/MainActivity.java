1fba19eaf982a297fee0f52d4629ac87aaf04c08
Larry1shen <shendezhou@gmail.com>
2018-03-01 22:28:19 -0800

    first commit

---
+++ app/src/main/java/com/example/w7_x64/hiworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.w7_x64.hiworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        fdsfjl//
+    }
+}
